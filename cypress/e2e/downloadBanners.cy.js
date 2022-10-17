0e5c160e680bb5c2c60c83f64318d8c76e77cabb
Bach Ly <bly@cochlear.com>
2022-10-18 10:08:30 +1100

    Download new banners

--- cypress/e2e/downloadBanners.cy.js
+++ cypress/e2e/downloadBanners.cy.js
@@ -3,25 +3,28 @@ const slugify = require('slugify');
 const $ = Cypress.$;
 
 const BASE_URL = 'https://www.ozsale.com.au';
+const NUM_BANNERS_TO_DOWNLOAD = 5;
 
 describe('Profile input', () => {
     before(() => {
-        cy.viewport(1920, 40000)
+        cy.viewport(1920, 2000)
         cy.visit(BASE_URL)
         cy.wait(10000);
     })
 
     it('download all banners from homepage', () => {
         cy.get('.test-banner-grid-img').each(($el, index, $list) => {
-            const largeImageUrl = $el.attr('srcset').split(',')[0].split(' ')[0];
-            const imageAlt = $el.attr('alt');
-            const handle = slugify(imageAlt,
-                {
-                    remove: /[*+~.()'"!:@]/g,
-                    strict: true,
-                    lower: true
-                })
-            cy.downloadFile(largeImageUrl, 'cypress/downloads/banners', `${handle}.jpeg`);
+            if (index < NUM_BANNERS_TO_DOWNLOAD) {
+                const largeImageUrl = $el.attr('srcset').split(',')[0].split(' ')[0];
+                const imageAlt = $el.attr('alt');
+                const handle = slugify(imageAlt,
+                    {
+                        remove: /[*+~.()'"!:@]/g,
+                        strict: true,
+                        lower: true
+                    })
+                cy.downloadFile(largeImageUrl, '_sampleData/banners', `${handle}.jpeg`);
+            }
         })
     })
 
